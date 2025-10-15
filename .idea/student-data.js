df91496c25c27b3974078dc6428c3974ecd7fb97
devonbadour <devonsw02@gmail.com>
2025-10-15 11:17:03 -0500

    using-html

---
+++ .idea/student-data.js
@@ -0,0 +1,19 @@
+
+export default [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe@mail.com',
+        department: 'Graphic Design',
+        dob: '09/03/1993',
+        hasGraduated: true
+    },
+    {
+        firstName: 'Lola',
+        lastName: 'Does',
+        email: 'lola.does',
+        department: 'UI / UX',
+        dob: '02/24/1990',
+        hasGraduated: false
+    }
+]
